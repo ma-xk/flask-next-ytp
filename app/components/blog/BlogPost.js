226a75e5011f257bf1f7898aaa688a8272f1940e
Scottsdaaale <scottsdaaale@gmail.com>
2024-01-25 13:55:12 -0500

    move blogpost into it's own component

---
+++ app/components/blog/BlogPost.js
@@ -0,0 +1,57 @@
+'use client'
+import React, { useEffect, useState } from 'react';
+import Image from 'next/image';
+import blogData from './blogData'
+import useFetchPost from '../common/useFetchPost';
+
+import blogPosts from '/app/components/blog/blogData';
+
+import '/app/components/blog/blog.css'; // Keep the CSS import
+
+
+const BlogPost = ({ params }) => {
+  const [post, setPost] = useState(null);
+
+  useEffect(() => {
+    const fetchedPost = blogPosts.find((p) => p.slug === params.slug);
+    setPost(fetchedPost);
+  }, [params.slug]);
+
+  if (!post) {
+    return (
+      <div className="container mx-auto">
+        <div className="flex justify-center">
+          <div className="w-full md:w-10/12">
+            <div>Post not found</div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <div className="container mx-auto">
+      <div className="flex justify-center">
+        <div className="w-full md:w-10/12">
+          <h1>{post.title}</h1>
+          {post.image && (
+            <Image
+              src={post.image}
+              alt={post.title}
+              className='blog-post-image'
+            />
+          )}
+          <p>
+            <strong>Author:</strong> {post.author}
+          </p>
+          <p>
+            <strong>Date:</strong> {post.date}
+          </p>
+          <div dangerouslySetInnerHTML={{ __html: post.text }}></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default BlogPost;
