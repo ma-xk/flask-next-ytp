82efe45f626705288c2a05a9089f589acdc9af2b
Scottsdaaale <scottsdaaale@gmail.com>
2024-01-25 13:54:53 -0500

    move blogpost into it's own component

--- app/(routes)/blog/[slug]/page.js
+++ app/(routes)/blog/[slug]/page.js
@@ -1,54 +1,14 @@
-'use client'
-import React, { useEffect, useState } from 'react';
-import Image from 'next/image';
+import React from 'react';
+import BlogPost from '../../../components/blog/BlogPost';
 
-import blogPosts from '/app/components/blog/blogData';
-
-import '/app/components/blog/blog.css'; // Keep the CSS import
-
-const BlogPost = ({ params }) => {
-  const [post, setPost] = useState(null);
-
-  useEffect(() => {
-    const fetchedPost = blogPosts.find((p) => p.slug === params.slug);
-    setPost(fetchedPost);
-  }, [params.slug]);
-
-  if (!post) {
-    return (
-      <div className="container mx-auto">
-        <div className="flex justify-center">
-          <div className="w-full md:w-10/12">
-            <div>Post not found</div>
-          </div>
-        </div>
-      </div>
-    );
-  }
-
+function page({ params }) {
   return (
-    <div className="container mx-auto">
-      <div className="flex justify-center">
-        <div className="w-full md:w-10/12">
-          <h1>{post.title}</h1>
-          {post.image && (
-            <Image
-              src={post.image}
-              alt={post.title}
-              className='blog-post-image'
-            />
-          )}
-          <p>
-            <strong>Author:</strong> {post.author}
-          </p>
-          <p>
-            <strong>Date:</strong> {post.date}
-          </p>
-          <div dangerouslySetInnerHTML={{ __html: post.text }}></div>
-        </div>
-      </div>
+    <div>
+      <BlogPost params={params}/>
     </div>
   );
-};
+}
 
-export default BlogPost;
+export default page;
+
+//we can now do dynamic metadata here
