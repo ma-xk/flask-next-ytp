8614c06acd2668780071406eb11e6819fa4d6153
Scottsdaaale <scottsdaaale@gmail.com>
2024-01-26 16:43:06 -0500

    added faq page

---
+++ app/(routes)/faq/page.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div className="container mx-auto">
+      <div className="flex justify-center">
+        <div className="w-full md:w-10/12 lg:w-10/12">
+        <h1>FAQ</h1>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default page
