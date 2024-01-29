d37fbb32668d062ab32d73b0ad9a6bbfa27069a3
Scottsdaaale <scottsdaaale@gmail.com>
2024-01-29 13:20:17 -0500

    format/fix path

--- app/components/homepage/Hero.js
+++ app/components/homepage/Hero.js
@@ -1,20 +1,24 @@
-import car from '/app/components/blog/car.png';
+import car from '../blog/car.png';
+import Image from 'next/image';
 
 const Hero = () => {
-    return (
-      <div className="flex justify-between items-center p-6">
-        <div className="space-y-4">
-          <h1 className="text-4xl font-bold">eLearning & Training Providers</h1>
-          <p className="text-gray-600">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-          <button className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-700 transition duration-300">
-            Click Me!
-          </button>
-        </div>
-        <div className="w-1/2 flex justify-center">
-        <img src={car} alt="Car Image" className="max-w-xs md:max-w-sm" />
-                </div>
+  return (
+    <div className='flex justify-between items-center p-6'>
+      <div className='space-y-4'>
+        <h1 className='text-4xl font-bold'>eLearning & Training Providers</h1>
+        <p className='text-gray-600'>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </p>
+        <button className='bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-700 transition duration-300'>
+          Click Me!
+        </button>
       </div>
-    );
-  };
-  
-  export default Hero;
+      <div className='w-1/2 flex justify-center'>
+        <Image src={car} alt='Car Image' className='max-w-xs md:max-w-sm' />
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
